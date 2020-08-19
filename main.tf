14f15ddbe067a3f03eced2bbb24bfb61f83cf1f0
Ubuntu <ubuntu@ip-172-31-39-108.eu-west-1.compute.internal>
2020-08-19 14:00:10 +0000

    main.tf now building EC2 instance inside subnet, inside VPC with internet gateway and route table set up

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region                  = var.region
   shared_credentials_file = var.credentials
-  }
+}
 
 resource "aws_vpc" "terraform_vpc" {
-  cidr_block = var.cidr_block
+  cidr_block           = var.cidr_block
   enable_dns_hostnames = true
 
   tags = {
@@ -17,14 +17,33 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_subnet" "terraform_subnet_a" {
-  vpc_id = aws_vpc.terraform_vpc.id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = aws_vpc.terraform_vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = data.aws_availability_zones.available.names[0]
 }
 
 resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance
-  key_name      = var.key
+  ami                         = var.ami
+  instance_type               = var.instance
+  key_name                    = var.key
+  subnet_id                   = aws_subnet.terraform_subnet_a.id
+  associate_public_ip_address = true
 }
 
+resource "aws_internet_gateway" "TFGW" {
+  vpc_id = aws_vpc.terraform_vpc.id
+}
+
+resource "aws_route_table" "TFRT" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.TFGW.id
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  route_table_id = aws_route_table.TFRT.id
+}
--- variables.tf
+++ variables.tf
@@ -1,33 +1,33 @@
 variable "instance" {
-    description = "This variable states the instance type for your EC2"
-    default = "t2.micro"
+  description = "This variable states the instance type for your EC2"
+  default     = "t2.micro"
 }
 
 variable "ami" {
-    description = "This variable states which ami to use for new EC2 instances"
-    default = "ami-07ee42ba0209b6d77"
+  description = "This variable states which ami to use for new EC2 instances"
+  default     = "ami-07ee42ba0209b6d77"
 }
 
 variable "region" {
-    description = "This variable states the default region for new EC2 instances"
-    default = "eu-west-1"
+  description = "This variable states the default region for new EC2 instances"
+  default     = "eu-west-1"
 }
 
 variable "key" {
-    description = "This variable states the key pair used for all new EC2 instances"
-    default = "mmcga"
+  description = "This variable states the key pair used for all new EC2 instances"
+  default     = "mmcga"
 }
 
 variable "credentials" {
-    description = "This variable states the location of the active credentials file"
-    default = "~/.aws/credentials"
+  description = "This variable states the location of the active credentials file"
+  default     = "~/.aws/credentials"
 }
 
 variable "cidr_block" {
-    default = "192.167.0.0/16"
+  default = "192.167.0.0/16"
 }
 
 variable "subnet_cidr_block" {
-    default = "192.167.1.0/24"
+  default = "192.167.1.0/24"
 }
 
