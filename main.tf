fbf822bc485910aa0ca125eff99599a30dd46ebe
Ubuntu <ubuntu@ip-172-31-39-108.eu-west-1.compute.internal>
2020-08-20 09:38:16 +0000

    completed modules for VPC and EC2. Also set up main.tf and variables.tf for SG module

--- main.tf
+++ main.tf
@@ -3,92 +3,11 @@ provider "aws" {
   shared_credentials_file = var.credentials
 }
 
-resource "aws_vpc" "terraform_vpc" {
-  cidr_block           = var.cidr_block
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "TFVPC"
-  }
+module "mmcga_vpc" {
+  source = "./VPC"
 }
 
-resource "aws_security_group" "Allow_SSH" {
-  name        = "Allow_SSH"
-  description = "Allows ability to SSH into EC2 instance"
-  vpc_id      = var.vpc_id
-
-  dynamic "ingress" {
-    iterator = port
-    for_each = var.ingress_ports
-    content {
-      from_port   = port.value
-      protocol    = "tcp"
-      to_port     = port.value
-      cidr_blocks = [var.internet]
-    }
-  }
-
-  egress {
-    from_port   = "0"
-    protocol    = "-1"
-    to_port     = "0"
-    cidr_blocks = [var.internet]
-  }
-
-  tags = {
-    Name = "Allow_SSH"
-  }
+module "mmcga_instance" {
+  source = "./EC2"
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-resource "aws_subnet" "terraform_subnet_a" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = data.aws_availability_zones.available.names[0]
-
-  tags = {
-    Name = "Subnet_a"
-  }
-}
-
-resource "aws_internet_gateway" "TFGW" {
-  vpc_id = var.vpc_id
-
-  tags = {
-    Name = "TFGW"
-  }
-}
-
-resource "aws_route_table" "TFRT" {
-  vpc_id = var.vpc_id
-
-  route {
-    cidr_block = var.internet
-    gateway_id = var.TFGW
-  }
-
-  tags = {
-    Name = "RT for terraform_vpc"
-  }
-}
-
-resource "aws_route_table_association" "TFRT_association" {
-  subnet_id      = var.subnet_id
-  route_table_id = aws_route_table.TFRT.id
-}
-
-resource "aws_instance" "web" {
-  ami                         = var.ami
-  instance_type               = var.instance
-  key_name                    = var.key
-  subnet_id                   = var.subnet_id
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.Allow_SSH.id]
-
-  tags = {
-    Name = "TFEC2"
-  }
-}
--- variables.tf
+++ variables.tf
@@ -1,40 +1,12 @@
-variable "instance" {
-  description = "This variable states the instance type for your EC2"
-  default     = "t2.micro"
-}
-
-variable "ami" {
-  description = "This variable states which ami to use for new EC2 instances"
-  default     = "ami-07ee42ba0209b6d77"
-}
-
 variable "region" {
   description = "This variable states the default region for new EC2 instances"
   default     = "eu-west-1"
 }
 
-variable "key" {
-  description = "This variable states the key pair used for all new EC2 instances"
-  default     = "mmcga"
-}
 
 variable "credentials" {
   description = "This variable states the location of the active credentials file"
   default     = "~/.aws/credentials"
-}
-
-variable "cidr_block" {
-  default = "192.167.0.0/16"
-}
-
-variable "subnet_cidr_block" {
-  default = "192.167.1.0/24"
-}
-
-variable "ingress_ports" {
-  type        = list
-  description = "list of ingress ports"
-  default     = [22]
 }
 
 variable "vpc_id" {
